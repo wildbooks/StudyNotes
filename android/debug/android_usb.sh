d89900016aca59939530c20a670d1c17ce6e484c
wildbook <www762268@foxmail.com>
2018-09-26 19:39:46 +0800

    new file:   Money/dd.md 	modified:   android/debug/android_usb.sh 	new file:   android/others/CountryCode.org 	deleted:    android/others/CountryCode.orq 	new file:   linux/05.commands/awesome-finder.org 	new file:   linux/05.commands/fdisk.org 	new file:   linux/05.commands/ip.org 	new file:   linux/05.commands/parallel.org 	renamed:    linux/08.server/how_to_buld_wordpress.org -> linux/08.server/how_to_build_wordpress.org

--- android/debug/android_usb.sh
+++ android/debug/android_usb.sh
@@ -1,3 +1,17 @@
+#1. adb root
+#     2. adb remount
+#     3. adb shell
+#     4. chmod 777 /dev/diag
+#     5. setenforce 0
+#     6. getenforce (confirm that its permissive)
+#     7. ps -A | grep “audio”
+#        （示例） i.e.
+#        audioserver 3669 1 39312 10644 binder_thread_read 0 S android.hardware.audio@2.0-service
+#        audioserver 3670 1 51620 11912 binder_thread_read 0 S audioserver
+#        Look for the process id for android.hardware.audio@2.0-service and kill the process
+#     8. kill 3669
+#     9. Now Connect to QACT
+
 # 开启USB调试端口
 echo   0 > /sys/class/android_usb/android0/enable
 echo  05c6 > /sys/class/android_usb/android0/idVendor
