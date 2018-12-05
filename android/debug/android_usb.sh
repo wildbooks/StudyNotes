1e4b68583124ba61a970dbc0f9f89e160159f272
yuchun.yang <yuchun.yang@archermind.com>
2018-12-05 10:16:00 +0800

    修改：     android/audio/AudioStructure.org 	修改：     android/audio/README.org 	修改：     android/audio/debug.org 	修改：     android/audio/fluence.org 	修改：     android/audio/method.org 	修改：     android/audio/systemsound.org 	修改：     android/debug/02.How_to_open_debug_log_in_qualcomm_platform.org 	修改：     android/debug/android_usb.sh 	新文件：   android/debug/systrace.org 	新文件：   android/input/framework/inputflinger.org 	新文件：   android/input/tools.org 	新文件：   android/others/crash.org 	修改：     android/others/setprop.org 	新文件：   android/power/regulator.org 	修改：     language/python/ReadMe.org 	修改：     linux/05.commands/repo.org

--- android/debug/android_usb.sh
+++ android/debug/android_usb.sh
@@ -13,8 +13,17 @@
 #     9. Now Connect to QACT
 
 # 开启USB调试端口
-echo   0 > /sys/class/android_usb/android0/enable
-echo  05c6 > /sys/class/android_usb/android0/idVendor
-echo 1 > /sys/class/android_usb/android0/enable
-start adbd
-sleep 10
+
+    #setprop sys.usb.config "diag,serial_smd,rmnet_qti_bam,adb" #这步手动执行
+
+    echo   0 > /sys/class/android_usb/android0/enable
+    echo "3fabf0f" > /sys/class/android_usb/android0/iSerial
+    echo "05C6" >/sys/class/android_usb/android0/idVendor
+    echo "9091" > /sys/class/android_usb/android0/idProduct
+    echo "diag" >/sys/class/android_usb/android0/f_diag/clients
+    echo "smd" > /sys/class/android_usb/android0/f_serial/transports
+    echo "qti,bam" >/sys/class/android_usb/android0/f_rmnet/transports
+    echo "diag,serial,rmnet,adb" >/sys/class/android_usb/android0/functions
+    echo   1 > /sys/class/android_usb/android0/enable
+    start adbd
+    setprop sys.usb.state "diag,serial_smd,rmnet_qti_bam,adb"
