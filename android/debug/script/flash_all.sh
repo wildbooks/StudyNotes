31bafea65a90aaeaedd86fd3a6634a548c210cb7
wildbook <www762268@foxmail.com>
2019-07-09 16:08:42 +0800

    修改：     android/debug/script/flash_all.sh

--- android/debug/script/flash_all.sh
+++ android/debug/script/flash_all.sh
@@ -1,396 +1,127 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#
-#    This file is auto create by scripts
-#    For download all img by fastboot
-#    2018/03/15
-#
-#    This file is auto create by scripts
-#    For download sku5 img by fastboot
-#    2018/03/22
-#
+function debug(){
+    case $1 in
+        cmd)
+            echo -e "$2"
+            ;;
+        info)#Green
+            echo -e "\033[0;32m$2\033[0m\t"
+            ;;
+        warn)#Yellow
+            echo -e "\033[0;33m$2\033[0m\t"
+            ;;
+        error)#Red
+            echo -e "\033[0;31mERROR: $2\033[0m\t"
+            ;;
+        *) WARN "debug:Don't have this choise!"
+    esac
+}
 
-# set path
-if [ -z "`which fastboot`" ] && [ -e "fastboot" ];then
-    for i in `echo $PATH | sed -e 's/:/ /g'`;do
-        cp -rf fastboot $i/fastboot > /dev/null
-    done
-    export PATH=`pwd`:$PATH
-fi
+function add_image(){
+    IMGDIC=(
+    #[abl]='abl.elf'
+    [modem]='NON-HLOS.bin'
+    [cache]="cache.img"
+    [boot]='boot.img'
+    [system]='system.img'
+    [vendor]='vendor.img'
+    [userdata]='userdata.img'
+    [recovery]='recovery.img'
+    [aboot]='emmc_appsboot.mbn'
+    )
+    IMGDIC["xbl_a"]="xbl.elf"
+    IMGDIC["xbl_b"]="xbl.elf"
+    IMGDIC["tz_a"]="tz.mbn"
+    IMGDIC["tz_b"]="tz.mbn"
+    IMGDIC["rpm_a"]="rpm.mbn"
+    IMGDIC["rpm_b"]="rpm.mbn"
+    IMGDIC["hyp_a"]="hyp.mbn"
+    IMGDIC["hyp_b"]="hyp.mbn"
+    IMGDIC["pmic_a"]="pmic.elf"
+    IMGDIC["pmic_b"]="pmic.elf"
+    IMGDIC["boot_a"]="boot.img"
+    IMGDIC["boot_b"]="boot.img"
+    IMGDIC["system_a"]="system.img"
+    IMGDIC["system_b"]="system.img"
+    IMGDIC["vendor_a"]="vendor.img"
+    IMGDIC["vendor_b"]="vendor.img"
+    IMGDIC["keymaster_a"]="km4.mbn"
+    IMGDIC["keymaster_b"]="km4.mbn"
+    IMGDIC["cmnlib_a"]="cmnlib.mbn"
+    IMGDIC["cmnlib64_a"]="cmnlib64.mbn"
+    IMGDIC["cmnlib_b"]="cmnlib.mbn"
+    IMGDIC["cmnlib64_b"]="cmnlib64.mbn"
+    IMGDIC["mdtpsecapp_a"]="mdtpsecapp.mbn"
+    IMGDIC["mdtpsecapp_b"]="mdtpsecapp.mbn"
+    IMGDIC["mdtp_a"]="mdtp.img"
+    IMGDIC["mdtp_b"]="mdtp.img"
+    IMGDIC["modem_a"]="NON-HLOS.bin"
+    IMGDIC["modem_b"]="NON-HLOS.bin"
+    IMGDIC["dsp_a"]="dspso.bin"
+    IMGDIC["dsp_b"]="dspso.bin"
+    IMGDIC["abl_a"]="abl.elf"
+    IMGDIC["abl_b"]="abl.elf"
+    IMGDIC["bluetooth_a"]="BTFM.bin"
+    IMGDIC["bluetooth_b"]="BTFM.bin"
+    IMGDIC["devcfg_a"]="devcfg.mbn"
+    IMGDIC["devcfg_b"]="devcfg.mbn"
+    IMGDIC["storsec"]="storsec.mbn"
+    IMGDIC["userdata"]="userdata.img"
+    IMGDIC["PrimaryGPT"]="gpt_main0.bin"
+    IMGDIC["BackupGPT"]="gpt_backup0.bin"
+    IMGDIC["persist"]="persist.img"
+    IMGDIC["sec"]="sec.dat"
+}
 
-# check fastboot tool
-if [ -z "`which fastboot`" ];then
-    echo -e "Error: No fastboot tool found !!!"
-    exit 1
-fi
+function flash_image(){
+    local img=$1
+    debug cmd "fastboot flash $img ${IMGDIC[$img]}"
+    #fastboot flash $img ${IMGDIC[$img]}
+    retVal=$?
 
-# default platform
-default_platform="8937"
+    if [ $retVal -ne 0 ]; then
+        local msg=$(perror $retVal)
+        debug error "===============${IMGDIC[$img]} : ${msg#*:}============="
+    fi
+    return $retVal
+}
 
-# default buildtype
-default_buildtype="eng"
+function unlock(){
+    # get build-type from fastboot command
+    buildtype=`fastboot getvar build-type 2>&1 | grep build-type | awk '{print $NF}'`
+    secret_key=`fastboot getvar secret-key-opt 2>&1 | grep secret-key-opt | awk '{print $NF}'`
 
+    if [ "$buildtype" = "user" ];then
+        echo $secret_key > default_key.bin
+        fastboot flash frp-unlock default_key.bin
+        fastboot flashing unlock
+        fastboot flashing unlock_critical
+    fi
+}
+
+function lock(){
+    # lock the device
+    fastboot flashing lock
+    fastboot flashing lock_critical
+}
+
+
+
+#-------------------- main -------------------------
 # get platform from fastboot command
 platform=`fastboot getvar platform 2>&1 | grep platform | awk '{print $NF}'`
+debug info "the CPU of this phone is $platform"
+declare -A IMGDIC
 
-# get build-type from fastboot command
-buildtype=`fastboot getvar build-type 2>&1 | grep build-type | awk '{print $NF}'`
+add_image
+for img in ${!IMGDIC[@]};
+do
+    flash_image $img || let 'total++'
+done
 
-secret_key=`fastboot getvar secret-key-opt 2>&1 | grep secret-key-opt | awk '{print $NF}'`
-
-if [ -z "$platform" ];then
-    platform="$default_platform"
+if [ "$total" == '' ]; then
+    debug info "-------------- flash sucessful --------------"
+else
+    debug error "-------------- flash fail:$total --------------"
 fi
-
-if [ -z "$buildtype" ];then
-    buildtype="$default_buildtype"
-fi
-
-if [ "$buildtype" = "user" ];then
-    echo $secret_key > default_key.bin
-    fastboot flash frp-unlock default_key.bin
-    fastboot flashing unlock
-    fastboot flashing unlock_critical
-fi
-
-platform="8937"
-
-# begin download
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_NON-HLOS.bin" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_NON-HLOS.bin\E[00m\n"
-    fastboot erase modem
-echo "flash id $?"
-    fastboot flash modem `dirname $0`/${platform}_NON-HLOS.bin
-echo "flash id $?"
-elif [ -e "NON-HLOS.bin" ];then
-    echo -e "\E[0;32mbegin fastboot download NON-HLOS.bin\E[00m\n"
-    fastboot erase modem
-    echo "flash id $?"
-    fastboot flash modem `dirname $0`/NON-HLOS.bin
-    echo "flash id $?"
-fi
-echo "flash id $?"
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_sbl1.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_sbl1.mbn\E[00m\n"
-    fastboot erase sbl1
-    echo "flash id $?"
-    fastboot flash sbl1 `dirname $0`/${platform}_sbl1.mbn
-    echo "flash id $?"
-elif [ -e "sbl1.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download sbl1.mbn\E[00m\n"
-    fastboot erase sbl1
-    fastboot flash sbl1 `dirname $0`/sbl1.mbn
-fi
-echo "flash id $?"
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_sbl1.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_sbl1.mbn\E[00m\n"
-    fastboot erase sbl1bak
-    fastboot flash sbl1bak `dirname $0`/${platform}_sbl1.mbn
-elif [ -e "sbl1.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download sbl1.mbn\E[00m\n"
-    fastboot erase sbl1bak
-    fastboot flash sbl1bak `dirname $0`/sbl1.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_rpm.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_rpm.mbn\E[00m\n"
-    fastboot erase rpm
-    echo "flash id $?"
-    fastboot flash rpm `dirname $0`/${platform}_rpm.mbn
-elif [ -e "rpm.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download rpm.mbn\E[00m\n"
-    fastboot erase rpm
-    echo "flash id $?"
-    fastboot flash rpm `dirname $0`/rpm.mbn
-    echo "flash id $?"
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_rpm.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_rpm.mbn\E[00m\n"
-    fastboot erase rpmbak
-    echo "flash id $?"
-    fastboot flash rpmbak `dirname $0`/${platform}_rpm.mbn
-    echo "flash id $?"
-elif [ -e "rpm.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download rpm.mbn\E[00m\n"
-    fastboot erase rpmbak
-    echo "flash id $?"
-    fastboot flash rpmbak `dirname $0`/rpm.mbn
-    echo "flash id $?"
-fi
-    echo "flash id $?"
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_tz.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_tz.mbn\E[00m\n"
-    fastboot erase tz
-    fastboot flash tz `dirname $0`/${platform}_tz.mbn
-elif [ -e "tz.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download tz.mbn\E[00m\n"
-    fastboot erase tz
-    fastboot flash tz `dirname $0`/tz.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_tz.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_tz.mbn\E[00m\n"
-    fastboot erase tzbak
-    fastboot flash tzbak `dirname $0`/${platform}_tz.mbn
-elif [ -e "tz.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download tz.mbn\E[00m\n"
-    fastboot erase tzbak
-    fastboot flash tzbak `dirname $0`/tz.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_devcfg.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_devcfg.mbn\E[00m\n"
-    fastboot erase devcfg
-    fastboot flash devcfg `dirname $0`/${platform}_devcfg.mbn
-elif [ -e "devcfg.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download devcfg.mbn\E[00m\n"
-    fastboot erase devcfg
-    fastboot flash devcfg `dirname $0`/devcfg.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_devcfg.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_devcfg.mbn\E[00m\n"
-    fastboot erase devcfgbak
-    fastboot flash devcfgbak `dirname $0`/${platform}_devcfg.mbn
-elif [ -e "devcfg.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download devcfg.mbn\E[00m\n"
-    fastboot erase devcfgbak
-    fastboot flash devcfgbak `dirname $0`/devcfg.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_adspso.bin" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_adspso.bin\E[00m\n"
-    fastboot erase dsp
-    fastboot flash dsp `dirname $0`/${platform}_adspso.bin
-elif [ -e "adspso.bin" ];then
-    echo -e "\E[0;32mbegin fastboot download adspso.bin\E[00m\n"
-    fastboot erase dsp
-    fastboot flash dsp `dirname $0`/adspso.bin
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_emmc_appsboot.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_emmc_appsboot.mbn\E[00m\n"
-    fastboot erase aboot
-    fastboot flash aboot `dirname $0`/${platform}_emmc_appsboot.mbn
-elif [ -e "emmc_appsboot.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download emmc_appsboot.mbn\E[00m\n"
-    fastboot erase aboot
-    fastboot flash aboot `dirname $0`/emmc_appsboot.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_emmc_appsboot.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_emmc_appsboot.mbn\E[00m\n"
-    fastboot erase abootbak
-    fastboot flash abootbak `dirname $0`/${platform}_emmc_appsboot.mbn
-elif [ -e "emmc_appsboot.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download emmc_appsboot.mbn\E[00m\n"
-    fastboot erase abootbak
-    fastboot flash abootbak `dirname $0`/emmc_appsboot.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_boot.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_boot.img\E[00m\n"
-    fastboot erase boot
-    fastboot flash boot `dirname $0`/${platform}_boot.img
-elif [ -e "boot.img" ];then
-    echo -e "\E[0;32mbegin fastboot download boot.img\E[00m\n"
-    fastboot erase boot
-    fastboot flash boot `dirname $0`/boot.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_recovery.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_recovery.img\E[00m\n"
-    fastboot erase recovery
-    fastboot flash recovery `dirname $0`/${platform}_recovery.img
-elif [ -e "recovery.img" ];then
-    echo -e "\E[0;32mbegin fastboot download recovery.img\E[00m\n"
-    fastboot erase recovery
-    fastboot flash recovery `dirname $0`/recovery.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_system.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_system.img\E[00m\n"
-    fastboot erase system
-    fastboot flash system `dirname $0`/${platform}_system.img
-elif [ -e "system.img" ];then
-    echo -e "\E[0;32mbegin fastboot download system.img\E[00m\n"
-    fastboot erase system
-    fastboot flash system `dirname $0`/system.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_cache.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_cache.img\E[00m\n"
-    fastboot erase cache
-    fastboot flash cache `dirname $0`/${platform}_cache.img
-elif [ -e "cache.img" ];then
-    echo -e "\E[0;32mbegin fastboot download cache.img\E[00m\n"
-    fastboot erase cache
-    fastboot flash cache `dirname $0`/cache.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_mdtp.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_mdtp.img\E[00m\n"
-    fastboot erase mdtp
-    fastboot flash mdtp `dirname $0`/${platform}_mdtp.img
-elif [ -e "mdtp.img" ];then
-    echo -e "\E[0;32mbegin fastboot download mdtp.img\E[00m\n"
-    fastboot erase mdtp
-    fastboot flash mdtp `dirname $0`/mdtp.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_cmnlib.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_cmnlib.mbn\E[00m\n"
-    fastboot erase cmnlib
-    fastboot flash cmnlib `dirname $0`/${platform}_cmnlib.mbn
-elif [ -e "cmnlib.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download cmnlib.mbn\E[00m\n"
-    fastboot erase cmnlib
-    fastboot flash cmnlib `dirname $0`/cmnlib.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_cmnlib.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_cmnlib.mbn\E[00m\n"
-    fastboot erase cmnlibbak
-    fastboot flash cmnlibbak `dirname $0`/${platform}_cmnlib.mbn
-elif [ -e "cmnlib.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download cmnlib.mbn\E[00m\n"
-    fastboot erase cmnlibbak
-    fastboot flash cmnlibbak `dirname $0`/cmnlib.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_cmnlib64.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_cmnlib64.mbn\E[00m\n"
-    fastboot erase cmnlib64
-    fastboot flash cmnlib64 `dirname $0`/${platform}_cmnlib64.mbn
-elif [ -e "cmnlib64.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download cmnlib64.mbn\E[00m\n"
-    fastboot erase cmnlib64
-    fastboot flash cmnlib64 `dirname $0`/cmnlib64.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_cmnlib64.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_cmnlib64.mbn\E[00m\n"
-    fastboot erase cmnlib64bak
-    fastboot flash cmnlib64bak `dirname $0`/${platform}_cmnlib64.mbn
-elif [ -e "cmnlib64.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download cmnlib64.mbn\E[00m\n"
-    fastboot erase cmnlib64bak
-    fastboot flash cmnlib64bak `dirname $0`/cmnlib64.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_keymaster.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_keymaster.mbn\E[00m\n"
-    fastboot erase keymaster
-    fastboot flash keymaster `dirname $0`/${platform}_keymaster.mbn
-elif [ -e "keymaster.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download keymaster.mbn\E[00m\n"
-    fastboot erase keymaster
-    fastboot flash keymaster `dirname $0`/keymaster.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_keymaster.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_keymaster.mbn\E[00m\n"
-    fastboot erase keymasterbak
-    fastboot flash keymasterbak `dirname $0`/${platform}_keymaster.mbn
-elif [ -e "keymaster.mbn" ];then
-    echo -e "\E[0;32mbegin fastboot download keymaster.mbn\E[00m\n"
-    fastboot erase keymasterbak
-    fastboot flash keymasterbak `dirname $0`/keymaster.mbn
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_userdata.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_userdata.img\E[00m\n"
-    fastboot erase userdata
-    fastboot flash userdata `dirname $0`/${platform}_userdata.img
-elif [ -e "userdata.img" ];then
-    echo -e "\E[0;32mbegin fastboot download userdata.img\E[00m\n"
-    fastboot erase userdata
-    fastboot flash userdata `dirname $0`/userdata.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_misc.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_misc.img\E[00m\n"
-    fastboot erase misc
-    fastboot flash misc `dirname $0`/${platform}_misc.img
-elif [ -e "misc.img" ];then
-    echo -e "\E[0;32mbegin fastboot download misc.img\E[00m\n"
-    fastboot erase misc
-    fastboot flash misc `dirname $0`/misc.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_APD.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_APD.img\E[00m\n"
-    fastboot erase APD
-    fastboot flash APD `dirname $0`/${platform}_APD.img
-elif [ -e "APD.img" ];then
-    echo -e "\E[0;32mbegin fastboot download APD.img\E[00m\n"
-    fastboot erase APD
-    fastboot flash APD `dirname $0`/APD.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_ADF.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_ADF.img\E[00m\n"
-    fastboot erase ADF
-    fastboot flash ADF `dirname $0`/${platform}_ADF.img
-elif [ -e "ADF.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ADF.img\E[00m\n"
-    fastboot erase ADF
-    fastboot flash ADF `dirname $0`/ADF.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_persist.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_persist.img\E[00m\n"
-    fastboot erase persist
-    fastboot flash persist `dirname $0`/${platform}_persist.img
-elif [ -e "persist.img" ];then
-    echo -e "\E[0;32mbegin fastboot download persist.img\E[00m\n"
-    fastboot erase persist
-    fastboot flash persist `dirname $0`/persist.img
-fi
-echo -e "\n------------------------------------------------------------"
-if [ -e "${platform}_splash.img" ];then
-    echo -e "\E[0;32mbegin fastboot download ${platform}_splash.img\E[00m\n"
-    fastboot erase splash
-    fastboot flash splash `dirname $0`/${platform}_splash.img
-elif [ -e "splash.img" ];then
-    echo -e "\E[0;32mbegin fastboot download splash.img\E[00m\n"
-    fastboot erase splash
-    fastboot flash splash `dirname $0`/splash.img
-fi
-
-# erase boot,force to reboot to bootloader
-# fastboot erase boot
-
-# reboot to bootloader
-# fastboot reboot bootloader
-
-# wait for device
-sleep 5
-
-# re unlock device
-#fastboot flash frp-unlock secret_key.bin
-#fastboot flashing unlock
-#fastboot flashing unlock_critical
-
-# redownload boot
-#if [ -e "${platform}_boot.img" ];then
-#    fastboot flash boot `dirname $0`/${platform}_boot.img
-#elif [ -e "boot.img" ];then
-#    fastboot flash boot `dirname $0`/boot.img
-#fi
-
-# lock the device
-#fastboot flashing lock
-#fastboot flashing lock_critical
-
-echo -e "\nAll is download,do you want to reboot(y/n) ?"
-read x
-if [ "$x" == "y" ] || [ "$x" == "Y" ];then
-    fastboot reboot
-fi
-
